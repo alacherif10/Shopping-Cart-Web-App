c040ad7f3d4cf79c99a6db7be6b694b022fc034d
alacherif10 <73743207+alacherif10@users.noreply.github.com>
2025-05-01 21:51:38 +0100

    Update ShoppingCartApplicationTests.java

--- src/test/java/com/reljicd/ShoppingCartApplicationTests.java
+++ src/test/java/com/reljicd/ShoppingCartApplicationTests.java
@@ -1,16 +1,13 @@
-package com.reljicd;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShoppingCartApplicationTests {
+@ActiveProfiles("test")
+class ShoppingCartApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Verify context starts successfully
+    }
 }
